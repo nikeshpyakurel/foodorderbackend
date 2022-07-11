8b62455ae280fd52326ff96423e5d1cec2a85064
riddhasoftchris <88536680+riddhasoftchris@users.noreply.github.com>
2022-07-12 01:01:49 +0545

    ok

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connect() {
   mongoose
-    .connect("mongodb://34.227.78.172:27017/resturantapp", {
+    .connect("mongodb://localhost:27017/resturantapp", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
